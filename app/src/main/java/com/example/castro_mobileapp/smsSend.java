9a831522e4ea1013515319f62bcfe191dad9c690
LAB11_USER ON WS14 <L11Y11W14@citulab.net>
2023-04-13 12:26:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/castro_mobileapp/smsSend.java
@@ -0,0 +1,42 @@
+package com.example.castro_mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class smsSend extends AppCompatActivity {
+
+    Button btnSend;
+    Button btnClear;
+    EditText txtPhone;
+    EditText txtMessage;
+    String phone;
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_send);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnSend = (Button) findViewById(R.id.btnSend);
+        txtPhone = (EditText) findViewById(R.id.txtPhoneNumber);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    txtPhone.setText("");
+                    txtMessage.setText("");
+                    Toast.makeText(smsSend.this, "Cleared", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
